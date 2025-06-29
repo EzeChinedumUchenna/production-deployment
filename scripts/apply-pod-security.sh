1c4a1f80abc0fc93b08d824cbe0a46723839e2d6
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2025-06-29 15:28:47 +0100

    update

---
+++ scripts/apply-pod-security.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+kubectl label namespace secure-apps \
+  pod-security.kubernetes.io/enforce=restricted \
+  pod-security.kubernetes.io/enforce-version=latest \
+  pod-security.kubernetes.io/audit=baseline \
+  pod-security.kubernetes.io/audit-version=latest \
+  pod-security.kubernetes.io/warn=baseline \
+  pod-security.kubernetes.io/warn-version=latest
