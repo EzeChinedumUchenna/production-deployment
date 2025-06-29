751b7e5d737247f6d449bcfd93ef6c2cfb56e659
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2025-06-29 07:17:34 +0100

    update

--- terraform/modules/minikube-vm/main.tf
+++ terraform/modules/minikube-vm/main.tf
@@ -102,31 +102,6 @@ resource "azurerm_network_security_group" "minikube_nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = var.ssh_allowed_ip_cidr # e.g. "YOUR.IP.ADDR.0/32."
-    destination_address_prefix = "*"
-  }
-
-  tags = {
-    Environment = "Dev"
-    Purpose     = "Allow SSH to VM"
-  }
-}
-
-
-resource "azurerm_network_security_group" "minikube_nsg" {
-  count               = var.create_nsg ? 1 : 0
-  name                = "minikube-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
     source_address_prefix      = var.ssh_allowed_ip_cidr # e.g. "YOUR.IP.ADDR.0/32"
     destination_address_prefix = "*"
   }
