ea5eaae9363aa8dd32bf9af8ee206a19a7ebd69e
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2025-06-29 15:13:28 +0100

    up

--- scripts/install-gatekeeper.sh
+++ scripts/install-gatekeeper.sh
@@ -1,3 +1,5 @@
 
 #!/bin/bash
 kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/release-3.12/deploy/gatekeeper.yaml
+kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper-library/master/library/pod-security-policy/privileged/template.yaml
+
